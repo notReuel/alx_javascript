57079a5843583ab673230e894c1af81f10e1a0d9
notReuel <northcottreuel@gmail.com>
2024-03-27 23:31:25 +0100

    4-completed - v1.0.1

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request')
+const fs - require('fs')
+
+let URL = process.argv[2]
+option = {
+    'method': 'GET',
+    'url': URL
+}
+
+let taskCompleted = {}
+
+request(options, (err,res) => {
+    if (err) console.log(err)
+    record = JSON.parse(res.body)
+    for(let i=0; i<record.length; i++){
+        if(record[i]['completed'] ){
+            if(record[i]['userId'] in taskCompleted){
+                taskCompleted[record[i]['userId']] += 1
+            }else {
+                taskCompleted[record[i]['userId']] = 1
+            }
+        }
+    }
+    console.log(taskCompleted)
+})
